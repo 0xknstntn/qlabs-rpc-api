0eedfbb843b537f3970b2aac1c57314cdd091a4f
knstntn <konstantin@quadrate.org>
2023-11-01 16:55:13 +0500

    Initial commit

---
+++ pages/api/[...keywords].js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+        if (req.method === 'GET') {
+                var url = "";
+                for (let index = 0; index < (req.query.keywords).length; index++) {
+                        url += "/" + req.query.keywords[index]
+                        
+                }
+                var data = await fetch("http://89.108.83.252:26657" + url, {
+                                        mode: 'cors',
+                                });
+
+
+                var dataJson = await data.json()
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(dataJson);
+        } else {
+        }
+}
