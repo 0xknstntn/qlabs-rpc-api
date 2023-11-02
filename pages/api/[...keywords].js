3c6ee62edf40b74e9291f0388601dfe91e723c06
knstntn <konstantin@quadrate.org>
2023-11-02 19:59:47 +0500

    feat: add POST

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -6,14 +6,34 @@ export default async function handler(req, res) {
                         
                 }
                 var data = await fetch("http://89.108.83.252:26657" + url, {
-                                        mode: 'cors',
-                                });
+                        mode: 'cors',
+                });
 
 
                 var dataJson = await data.json()
                 res.setHeader('Access-Control-Allow-Origin', '*');
                 res.setHeader('Content-Type', 'application/json');
                 res.status(200).json(dataJson);
-        } else {
+        } 
+
+        if (req.method === 'POST') {
+                var url = "";
+                for (let index = 0; index < (req.query.keywords).length; index++) {
+                        url += "/" + req.query.keywords[index]
+                        
+                }
+                var data = await fetch("http://89.108.83.252:26657" + url, {
+                        method: 'POST',
+                        headers: {
+                                'Content-Type': 'application/json',
+                        },
+                        mode: 'cors',
+                });
+
+
+                var dataJson = await data.json()
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(dataJson);
         }
 }
