7db9c9981752ee5346b27685d245dff5cb5c6b50
knstntn <konstantin@quadrate.org>
2023-11-03 17:18:09 +0500

    feat: remove headers & add mode

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -21,24 +21,19 @@ export default async function handler(req, res) {
         if (req.method === 'POST') {
 
                 const body = JSON.parse(req.body)
-
+                console.log(body)
                 var data = await fetch("http://89.108.83.252:26657", {
                         method: 'POST',
                         headers: {
-                                'Access-Control-Allow-Origin': '*',
                                 'Content-Type': 'application/json',
                         },
                         body: JSON.stringify(body),
+                        mode: 'cors',
                 });
 
                 var dataJson = await data.json()
-                res.setHeader('Access-Control-Allow-Credentials', true)
-                res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
-                res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-                res.setHeader(
-                        'Access-Control-Allow-Headers',
-                        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-                )
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'application/json');
                 res.status(200).json(dataJson);
         }
 }
