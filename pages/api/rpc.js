e8dfa3d285ed6d84ff12daf374abfbb9cbf1d387
knstntn <konstantin@quadrate.org>
2023-11-03 23:24:40 +0500

    feat: add rpc and rest pages

---
+++ pages/api/rpc.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+        if (req.method === 'POST') {
+                var data = await fetch("http://89.108.83.252:26657", {
+                        method: 'POST',
+                        headers: {
+                                'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(req.body),
+                });
+
+                var dataJson = await data.json()
+                console.log(dataJson)
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'application/json');
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                res.status(200).json(dataJson);
+        }
+}
