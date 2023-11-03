bf65dcfe68ed89926fc72289d1139a3573a14493
knstntn <konstantin@quadrate.org>
2023-11-03 18:03:00 +0500

    fix

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -1,4 +1,5 @@
-export async function GET(req, res) {
+export async function handler(req, res) {
+        if (req.method === 'GET') {
                 var url = "";
                 if ((req.query.keywords).length != 0) {
                         for (let index = 0; index < (req.query.keywords).length; index++) {
@@ -15,20 +16,24 @@ export async function GET(req, res) {
                 res.setHeader('Access-Control-Allow-Origin', '*');
                 res.setHeader('Content-Type', 'application/json');
                 res.status(200).json(dataJson);
-}
-export async function POST(req, res) {
-        const body = JSON.parse(req.body)
-        var data = await fetch("http://89.108.83.252:26657", {
-                method: 'POST',
-                headers: {
-                        'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body),
-                mode: 'cors',
-        });
+        } 
 
-        var dataJson = await data.json()
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(dataJson);
+        if (req.method === 'POST') {
+
+                const body = JSON.parse(req.body)
+                console.log(body)
+                var data = await fetch("http://89.108.83.252:26657", {
+                        method: 'POST',
+                        headers: {
+                                'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(body),
+                        mode: 'cors',
+                });
+
+                var dataJson = await data.json()
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(dataJson);
+        }
 }
