9ce774ad83d7d76f9d852505e557aecf2816ffb0
knstntn <konstantin@quadrate.org>
2023-11-03 16:33:52 +0500

    fix: cors header

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -32,10 +32,10 @@ export default async function handler(req, res) {
                 });
 
                 var dataJson = await data.json()
-                res.headers.append('Access-Control-Allow-Credentials', "true")
-                res.headers.append('Access-Control-Allow-Origin', '*')
-                res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-                res.headers.append(
+                res.setHeader('Access-Control-Allow-Credentials', true)
+                res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+                res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+                res.setHeader(
                         'Access-Control-Allow-Headers',
                         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
                 )
