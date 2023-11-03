944354b277b2f68f9273ef44c4842b78e7d95aeb
knstntn <konstantin@quadrate.org>
2023-11-03 17:20:43 +0500

    fix

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-        if (req.method === 'GET') {
+export async function GET(req, res) {
                 var url = "";
                 if ((req.query.keywords).length != 0) {
                         for (let index = 0; index < (req.query.keywords).length; index++) {
@@ -16,24 +15,20 @@ export default async function handler(req, res) {
                 res.setHeader('Access-Control-Allow-Origin', '*');
                 res.setHeader('Content-Type', 'application/json');
                 res.status(200).json(dataJson);
-        } 
+}
+export async function POST(req, res) {
+        const body = JSON.parse(req.body)
+        var data = await fetch("http://89.108.83.252:26657", {
+                method: 'POST',
+                headers: {
+                        'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+                mode: 'cors',
+        });
 
-        if (req.method === 'POST') {
-
-                const body = JSON.parse(req.body)
-                console.log(body)
-                var data = await fetch("http://89.108.83.252:26657", {
-                        method: 'POST',
-                        headers: {
-                                'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(body),
-                        mode: 'cors',
-                });
-
-                var dataJson = await data.json()
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Content-Type', 'application/json');
-                res.status(200).json(dataJson);
-        }
+        var dataJson = await data.json()
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(dataJson);
 }
