30d792d5a1eb23dae54b8e46d827aa941c7c26a8
knstntn <konstantin@quadrate.org>
2023-11-03 15:50:15 +0500

    feat: refactor POST

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -19,26 +19,22 @@ export default async function handler(req, res) {
         } 
 
         if (req.method === 'POST') {
-                /*var url = "";
-                if ((req.query.keywords).length != 0) {
-                        for (let index = 0; index < (req.query.keywords).length; index++) {
-                                url += "/" + req.query.keywords[index]
-                        }
-                }*/
-                const { body } = req
+
+                const body = JSON.parse(req.body)
+
                 var data = await fetch("http://89.108.83.252:26657", {
                         method: 'POST',
                         headers: {
                                 'Content-Type': 'application/json',
                         },
-                        mode: 'cors',
-                        body,
+                        mode: "cors",
+                        body: JSON.stringify(body),
                 });
-
 
                 var dataJson = await data.json()
                 res.setHeader('Access-Control-Allow-Origin', '*');
                 res.setHeader('Content-Type', 'application/json');
                 res.status(200).json(dataJson);
+                res.status(771).send({ message: 'Only POST requests allowed' })
         }
 }
