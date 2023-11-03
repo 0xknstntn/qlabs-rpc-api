346d334001e3ff81a81738d831d29d113e9a1738
knstntn <konstantin@quadrate.org>
2023-11-03 23:42:32 +0500

    feat: all method works

---
+++ pages/api/[...keywords].js
@@ -0,0 +1,39 @@
+export default async function handler(req, res) {
+        switch (req.method) {
+                case "POST":
+                        var data = await fetch("http://89.108.83.252:26657", {
+                                method: 'POST',
+                                headers: {
+                                        'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(req.body),
+                        });
+
+                        var dataJson = await data.json()
+                        console.log(dataJson)
+                        res.setHeader('Access-Control-Allow-Origin', '*');
+                        res.setHeader('Content-Type', 'application/json');
+                        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                        res.status(200).json(dataJson);
+                        break;
+        
+                case "GET":
+                        var url = "";
+                        if ((req.query.keywords).length != 0) {
+                                for (let index = 0; index < (req.query.keywords).length; index++) {
+                                        url += "/" + req.query.keywords[index]
+                                }
+                        }
+                        
+                        var data = await fetch("http://89.108.83.252:26657" + url, {
+                                mode: 'cors',
+                        });
+
+
+                        var dataJson = await data.json()
+                        res.setHeader('Access-Control-Allow-Origin', '*');
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(200).json(dataJson);
+                        break;
+        }
+}
