3f0dd785c8647e825c3c5d050504d41833cbb91d
knstntn <konstantin@quadrate.org>
2023-11-03 23:56:32 +0500

    feat: add mode in fetch

--- pages/api/[...keywords].js
+++ pages/api/[...keywords].js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
                                         'Content-Type': 'application/json',
                                 },
                                 body: JSON.stringify(req.body),
+                                mode: 'cors',
                         });
 
                         var dataJson = await data.json()
