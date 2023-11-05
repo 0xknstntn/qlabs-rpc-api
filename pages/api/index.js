2ea03439a475196b1eb4701d6fda63b74c58a051
knstntn <konstantin@quadrate.org>
2023-11-05 14:55:19 +0500

    feat: add OPTIONS method

--- pages/api/index.js
+++ pages/api/index.js
@@ -18,5 +18,23 @@ export default async function handler(req, res) {
                         res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token')
                         res.status(200).json(dataJson);
                         break;
+                case "OPTIONS":
+                        var data = await fetch("http://89.108.83.252:26657", {
+                                method: 'POST',
+                                headers: {
+                                        'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(req.body),
+                                mode: 'cors',
+                        });
+
+                        var dataJson = await data.json()
+                        console.log(dataJson)
+                        res.setHeader('Access-Control-Allow-Origin', '*');
+                        res.setHeader('Content-Type', 'application/json');
+                        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                        res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token')
+                        res.status(200).json(dataJson);
+                        break;
         }
 }
