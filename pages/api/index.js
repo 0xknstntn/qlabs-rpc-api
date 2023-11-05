e28848a11076869133599f106ba88565f39680c9
knstntn <konstantin@quadrate.org>
2023-11-05 14:04:39 +0500

    feat: add Access-Control-Allow-Methods

--- pages/api/index.js
+++ pages/api/index.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
                         res.setHeader('Access-Control-Allow-Origin', '*');
                         res.setHeader('Content-Type', 'application/json');
                         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                         res.status(200).json(dataJson);
                         break;
         }
